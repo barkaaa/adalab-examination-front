0c29a68c9a68eafc9b8ea6309993495047df3711
karl-von <99388708@qq.com>
2022-08-23 09:39:13 +0800

    count down and button

--- src/views/MarkdownChallenge.vue
+++ src/views/MarkdownChallenge.vue
@@ -1,10 +1,26 @@
 <template>
 markdown闯关
+  <a-space>
+    <a-button type="primary" :loading="loading" @click="handleClick" shape="round" >{{btm}}</a-button>
+  </a-space>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
-  name: "MarkdownChallenge"
+  name: "MarkdownChallenge",
+  data() {
+    return {
+      loading : ref(false),
+      btm:'点击提交'
+    }
+  },
+  methods:{
+    handleClick(){
+      this.loading= !this.loading
+      this.btm = '正在测评'
+    }
+  },
 }
 </script>
 
