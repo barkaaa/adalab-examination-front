c0af265e9db24aa13edb7aebe1796c6bf37546a9
cao-cyc <>
2022-08-23 12:21:36 +0800

    排名组件更新x3

--- src/components/RankingPlugin.vue
+++ src/components/RankingPlugin.vue
@@ -1,42 +1,79 @@
 <template>
-  <h1>进度条</h1>
 
-<p>1</p>
-<div class="container">
-  <div class="skills html">90%</div>
+<!-- <p>1</p>
+  <div class="container">
+    <div class="skills html">90%</div>
+  </div>
+
+  <p>2</p>
+  <div class="container">
+    <div class="skills css">80%</div>
+  </div>
+
+  <p>3</p>
+  <div class="container">
+    <div class="skills js">65%</div>
+  </div>
+
+  <p>李根宝</p>
+  <div class="container">
+    <div class="skills php">60%</div>
+  </div> -->
+
+  
+  <!-- <p v-for="(item, i) in users">{{ i }}huihuihui{{ item }}</p> -->
+
+  <div v-for="(item, i) in users">
+  <p>{{item.name}}</p>
+  <div class="container">
+    <div class="clearNum" v-bind:style="{width:item.clear+'%'}" v-if:style="{color:pink}">
+      {{item.clear}}
+    </div>
+  </div>
+  </div>
+<div v-for="(item, i) in users">
+    <a-progress
+      size='large'
+      :percent="1"
+      :style="{ width: '50%' }"
+      :color="{
+        '0%': 'rgb(var(--primary-6))',
+        '100%': 'rgb(var(--success-6))',
+      }"
+    />
+    <br/>
+    <br/>
 </div>
 
-<p>2</p>
-<div class="container">
-  <div class="skills css">80%</div>
-</div>
 
-<p>3</p>
-<div class="container">
-  <div class="skills js">65%</div>
-</div>
 
-<p>李根宝</p>
-<div class="container">
-  <div class="skills php">60%</div>
-</div>
-<p v-for="(item,i) in users">{{i}}huihuihui{{item}}</p>
+
+
+
 </template>
 
 <script>
 export default {
-  name: 'RankingPlugin',
+  name: "RankingPlugin",
   props: {
-    users:Array,
-  }
-}
+    users: Array,
+    
+  },data() {
+    return {
+      color:"red",
+      number:this.users[1].clear+"%",
+      user: this.users
+      
+    }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-
-* {box-sizing: border-box}
+* {
+  box-sizing: border-box;
+}
 
 .container {
   width: 100%;
@@ -50,10 +87,27 @@ export default {
   color: white;
 }
 
-.html {width: 90%; background-color: #4CAF50;}
-.css {width: 80%; background-color: #2196F3;}
-.js {width: 65%; background-color: #f44336;}
-.php {width: 60%; background-color: #808080;}
-
-
+.html {
+  width: 90%;
+  background-color: #4caf50;
+}
+.css {
+  width: 80%;
+  background-color: #2196f3;
+}
+.js {
+  width: 65%;
+  background-color: #f44336;
+}
+.php {
+  width: v-bind(number);
+  background-color: #808080;
+}
+.clearNum{
+  background: linear-gradient(90deg,yellow,blue);
+  
+}
+a-progress{
+  text-align: center;
+}
 </style>
--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -1,30 +1,32 @@
 <template>
-<h1>1:</h1>
-<RankingPlugin v-bind:users="users">123</RankingPlugin>
-<!-- <p v-for="(item,i) in list">{{i}},{{item}}</p> -->
-
+  
+  <RankingPlugin v-bind:users="users">123</RankingPlugin>
+  <!-- <p v-for="(item,i) in list">{{i}},{{item}}</p> -->
 </template>
 
 <script>
-import RankingPlugin from '@/components/RankingPlugin.vue';
+import RankingPlugin from "@/components/RankingPlugin.vue";
 
 export default {
-  name: 'Ranking',
-  data(){
-    return{
-        users:[1,2,3,4,5,6],
-    }
-   },
+  name: "Ranking",
+  data() {
+    return {
+      users: [
+        { name: "王狗剩", clear: 7 },
+        { name: "李根宝", clear: 9 },
+        { name: "David", clear: 1 },
+      ],
+    };
+  },
 
-//   props: {
-//     img: String,
-//     username:String,
-//     rank:String
-//   }
-components:{RankingPlugin}
-}
+  //   props: {
+  //     img: String,
+  //     username:String,
+  //     rank:String
+  //   }
+  components: { RankingPlugin },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
