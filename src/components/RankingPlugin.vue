6fb38da5111881dc277b41824e6521cf520cd49d
cao-cyc <>
2022-08-26 20:08:57 +0800

    删除了多余的模态框

--- src/components/RankingPlugin.vue
+++ src/components/RankingPlugin.vue
@@ -15,9 +15,7 @@
   <h1 id="rankingList">排行榜</h1>
   <div class="list">
     <div class="outer-container" v-for="(item, i) in users">
-    <a  class="btn" href="#open-modal"
-            ><p class="name">{{ item.name }}</p></a
-          >
+    <p class="name">{{ item.name }}</p>
       <div class="container">
         <div
           class="progress"
@@ -132,60 +130,7 @@ a {
   size: large;
   
 } */
-/* 模态框 */
-.modal-window {
-  position: fixed;
-  /* background-color: rgba(255, 255, 255, 0.25); */
-  background-color: rgba(0, 0, 0, 0.25);
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 999;
-  visibility: hidden;
-  opacity: 0;
-  pointer-events: none;
-  -webkit-transition: all 0.3s;
-  transition: all 0.3s;
-}
-.modal-window:target {
-  visibility: visible;
-  opacity: 1;
-  pointer-events: auto;
-}
-.modal-window > div {
-  width: 80vw;
-  height: 80vh;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  padding: 2em;
-  background: #ffffff;
-}
-.modal-window header {
-  font-weight: bold;
-}
-.modal-window h1 {
-  font-size: 150%;
-  margin: 0 0 15px;
-}
 
-.modal-close {
-  color: #aaa;
-  line-height: 50px;
-  font-size: 80%;
-  position: absolute;
-  right: 0;
-  text-align: center;
-  top: 0;
-  width: 70px;
-  text-decoration: none;
-}
-.modal-close:hover {
-  color: black;
-}
 /* .btn{
   width: 1vh;
 } */
