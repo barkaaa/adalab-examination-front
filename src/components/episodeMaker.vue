a649828cd3ab601a77be4ce981eae05c33064e54
yay <>
2022-08-28 14:57:33 +0800

    更新关卡配置上次界面(样式未设置)

--- src/components/episodeMaker.vue
+++ src/components/episodeMaker.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <input type="text" v-model="id">
-    <input type="text" v-model="files">
-    <input type="text" v-model="timeOut">
-    <input type="text" v-model="img">
-    <input type="text" v-model="cmd">
-    <input type="text" v-model="testRequired">
-    <input type="file" @change="getFile($event)">
+    <select v-model="testRequired">
+      <option value="true">需要测试</option>
+      <option value="false">不需要测试</option>
+    </select>
+    <input v-if="testRequired" type="text" v-model="timeOut">
+    <select v-if="testRequired" v-model="img">
+      <option v-for="image in images" :key="image.Id" :value="image.Id">
+        {{ image.RepoTags }}}
+      </option>
+    </select>
+    <input v-if="testRequired" type="text" v-model="cmd">
+    <input v-if="testRequired" type="file" @change="getFile($event)" multiple="multiple">
     <button @click="submit($event)">提交</button>
   </div>
 </template>
@@ -20,27 +26,39 @@ export default {
       files: {},
       timeOut: '',
       cmd: '',
-      testRequired: '',
+      testRequired: false,
       img: '',
       images: {}
     }
   },
   methods: {
     getFile(event) {
-      this.file = event.target.files[0];
+      this.files = event.target.files;
     },
 
     submit(event) {
       event.preventDefault();
       let formData = new FormData();
-
+      let ep = {
+        id: this.id,
+        cmd: this.cmd,
+        timeOut: this.timeOut,
+        imgId: this.img,
+        testRequired: this.testRequired
+      };
       let config = {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
-      }
+      };
 
-      this.axios.post('/api/episode/docker', formData, config).then(function (res) {
+      formData.append("episode", new Blob([JSON.stringify(ep)], {type: 'application/json'}));
+      Array.prototype.forEach.call(this.files, function (file) {
+        formData.append("test", file)
+      });
+
+
+      this.axios.post('/api/episode/episode', formData, config).then(function (res) {
         console.log(res.data);
       })
 
