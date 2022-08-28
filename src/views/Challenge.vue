7b5bfa1cf5f2a8750f48848511d4a7b55fccd4f3
雄霸天下 <bighubaba16@outlook.com>
2022-08-28 20:09:46 +0800

    完善challenge common 的页面选项

--- src/views/Challenge.vue
+++ src/views/Challenge.vue
@@ -1,29 +1,50 @@
 <template>
   <div class="box">
-    <div class="problem_box" v-for="item in res">
+    <div class="problem_box" v-for="(item, index) in res">
       <h3>{{ item.theme }}</h3>
       <a-textarea
+        v-model="answer[index].fill"
         placeholder="请在这里输入"
         allow-clear
         auto-size
         v-if="item.questionType === 1"
       />
 
-      <a-radio-group v-if="item.isMultiple === 'false'">
-        <a-radio
-          :value="option"
-          v-if="item.isMultiple === 'false' && item.questionType === 2"
-          v-for="option in item.options.split('?').slice(0, -1)"
+      <div v-if="item.isMultiple === 'false' && item.questionType === 2">
+        <a-radio-group
+          v-if="item.isMultiple === 'false'"
+          v-model="answer[index].selectOptions[0]"
         >
-          {{ option }}
-        </a-radio>
-      </a-radio-group>
-      <a-checkbox
-        :value="1"
-        v-for="option in item.options.split('?').slice(0, -1)"
-        v-if="item.isMultiple === 'true' && item.questionType === 2"
-        >{{ option }}
-      </a-checkbox>
+          <a-radio
+            :value="option"
+            v-for="option in item.options.split('?').slice(0, -1)"
+          >
+            {{ option }}
+          </a-radio>
+          <a-radio value="自己填选项">自己填选项</a-radio>
+        </a-radio-group>
+        <a-divider />
+      </div>
+
+      <div v-if="item.isMultiple === 'true' && item.questionType === 2">
+        <a-checkbox-group v-model="answer[index].selectOptions">
+          <a-checkbox
+            :value="option"
+            v-for="(option, x) in item.options.split('?').slice(0, -1)"
+            >{{ option }}
+          </a-checkbox>
+          <a-checkbox value="自己填选项">自己填选项 </a-checkbox>
+        </a-checkbox-group>
+        <a-divider />
+      </div>
+
+      <a-textarea
+        v-model="answer[index].fill"
+        placeholder="自己填选项"
+        allow-clear
+        auto-size
+        v-if="item.questionType == 2 && item.isAddtional === 'false'"
+      />
     </div>
   </div>
   <div class="box">
@@ -34,10 +55,12 @@
 <script>
 import { useChallengeStore } from "../store/challenge";
 import { storeToRefs } from "pinia";
-import { ref, onBeforeMount, getCurrentInstance } from "vue";
+import { ref, onBeforeMount, getCurrentInstance, reactive } from "vue";
 
 export default {
   setup() {
+    var answer = reactive([]);
+
     const challenge = useChallengeStore();
     let { cur } = storeToRefs(challenge);
 
@@ -54,27 +77,34 @@ export default {
         params: { missionNum: cur.value },
       });
       res.value = r.data;
+      initAnswer();
     });
+
+    const initAnswer = () => {
+      for (const key in res) {
+        answer.push({ fill: "", selectOptions: [] });
+      }
+    };
+
     return {
       bLoading,
       nextChallenge,
       res,
       cur,
       challenge,
+      answer,
+      initAnswer,
     };
   },
   data() {
-  
     return {
       content: " ",
     };
-
-    
   },
   methods: {
-      test(){
-        console.log(111);
-      },
+    test() {
+      console.log(this.answer);
+    },
   },
   async mounted() {
     let res = await this.axios.get("/api/levels/getone", {
@@ -84,7 +114,6 @@ export default {
     let md = (await this.axios.get(url)).data;
     this.content = this.markded.parse(md);
   },
-  
 };
 </script>
 
